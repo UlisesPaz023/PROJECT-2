b1a50fcbd2aa392fc5a5fc23af77a2d6f9cef0d3
Ulises <lioipaz2010@gmail.com>
2023-02-20 11:33:44 -0300

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 let cardDinamic = document.getElementById('cardDinamic')
-let juegos = [
-{
-  id : 0,
-  category: 'ESTRATEGIA',
-  name: 'Desesperados III',
-  description: 'Money for the vultures',
-  price: 2.45,
-  img:'./img/card1.jpg'
-},
-{
-  id : 1,
-  category: 'ESTRATEGIA',
-  name: 'Thronebreaker',
-  description: 'The witcher tales',
-  price: 2.45,
-  img:'./img/card2.jpg'
-}
-]
+let tableSection = document.getElementById('tableSection')
+
+import {games} from './games.js'
+
+games.map((x)=>{
+  if(x.id % 4 === 0){
+    cardDinamic.innerHTML += 
+    `
+    <div class="cardsTitle w-100 text-light text-center fw-bold mt-5">
+    <h3 class="cardsTitleText mx-2 my-4 pb-1 border-bottom">${x.category}</h3>
+    </div>
+    `
+  }
+  cardDinamic.innerHTML +=
+  `
+  <a href="#" class="card m-2 card p-0 border-1 rounded-0">
+  <figure class="stars position-absolute ps-2">
+  <i class="bi bi-star-fill text-warning"></i>
+  <i class="bi bi-star-fill text-warning"></i>
+  <i class="bi bi-star-fill text-warning"></i>
+  <i class="bi bi-star-fill text-warning"></i>
+  <i class="bi bi-star text-warning"></i>
+  </figure>
+  <img src="${x.img}" class="cardImg card-img-top bg-danger rounded-0" alt="img${x.id}">
+  <div class="position-absolute bottom-0 w-100 cardbodySize card-body d-flex flex-column justify-content-between p-0 pt-2 px-2 bg-dark text-light">
+  <div class="">
+  <h4 class="card-title m-0">${x.name}</h4>
+      <p class="cardText card-text">${x.description}</p>
+    </div>
+    <div class="cardFooter d-flex align-items-center justify-content-between">
+    <h2>$${x.price}</h2>
+      <div class="me-1">
+        <i class="bi bi-playstation"></i>
+        <i class="bi bi-windows"></i>
+        </div>
+    </div>
+    </div>
+    </a>
+      `
+  // tableSection.innerHTML += 
+  // `
+  // <tr>
+  //   <th scope="row">${x.id}</th>
+  //   <td>${x.name}</td>
+  //   <td>${x.category}</td>
+  //   <td>${x.description}</td>
+  //   <td>${x.published}</td>
+  //   <td></td>
+  // </tr>
+  // `
+});
 
 const card = document.querySelectorAll('.card')
 const stars = document.querySelectorAll('.stars')
@@ -24,37 +57,6 @@ const cardbodySize = document.querySelectorAll('.cardbodySize')
 const cardFooter = document.querySelectorAll('.cardFooter')
 const cardImg = document.querySelectorAll('.cardImg')
 
-juegos.map((x)=>{
-  cardDinamic.innerHTML +=
-    `
-    <a href="#" class="card m-2 card p-0 border-1 rounded-0">
-    <figure class="stars position-absolute ps-2">
-      <i class="bi bi-star-fill text-warning"></i>
-      <i class="bi bi-star-fill text-warning"></i>
-      <i class="bi bi-star-fill text-warning"></i>
-      <i class="bi bi-star-fill text-warning"></i>
-      <i class="bi bi-star text-warning"></i>
-    </figure>
-    <img src="${x.img}" class="cardImg card-img-top bg-danger rounded-0" alt="img${x.id}">
-    <div class="position-absolute bottom-0 w-100 cardbodySize card-body d-flex flex-column justify-content-between p-0 pt-2 px-2 bg-dark text-light">
-      <div class="">
-        <h4 class="card-title m-0">${x.name}</h4>
-        <p class="cardText card-text">${x.description}</p>
-      </div>
-      <div class="cardFooter d-flex align-items-center justify-content-between">
-        <h2>$${x.price}</h2>
-        <div class="me-1">
-          <i class="bi bi-playstation"></i>
-          <i class="bi bi-windows"></i>
-        </div>
-      </div>
-    </div>
-    </a>
-    `;
-});
-
-
-//card Hover
 card.forEach((eachCard, i)=>{
   card[i].addEventListener('mouseenter',()=>{
     stars[i].style.transitionDelay = '.3s';
---
+++ games.js
@@ -0,0 +1,163 @@
+
+export let games = [
+  {
+    id : 0,
+    category: 'ESTRATEGIA',
+    name: 'Desesperados III',
+    description: 'Money for the vultures',
+    price: 2.45,
+    img:'./img/card1.jpg',
+    published: '12/12/12',
+    outstanding: true 
+  },
+  {
+    id : 1,
+    category: 'ESTRATEGIA',
+    name: 'Thronebreaker',
+    description: 'The witcher tales',
+    price: 2.45,
+    img:'./img/card2.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 2,
+    category: 'ESTRATEGIA',
+    name: 'Civilization',
+    description: 'New Frontier Pass',
+    price: 2.45,
+    img:'./img/card3.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 3,
+    category: 'ESTRATEGIA',
+    name: 'Hitman III',
+    description: 'Hitman III',
+    price: 2.45,
+    img:'./img/card4.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 4,
+    category: 'LUCHA',
+    name: 'Street Fighter V',
+    description: 'Arcade Edition',
+    price: 2.45,
+    img:'./img/card5.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 5,
+    category: 'LUCHA',
+    name: 'Mortal Kombat XI',
+    description: 'Ultimate',
+    price: 2.45,
+    img:'./img/card6.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 6,
+    category: 'LUCHA',
+    name: 'The King Of Fighters XV',
+    description: 'Ultimate',
+    price: 2.45,
+    img:'./img/card7.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 7,
+    category: 'LUCHA',
+    name: 'Tekken 7',
+    description: 'Tekken 7',
+    price: 2.45,
+    img:'./img/card8.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 8,
+    category: 'CARRERA',
+    name: 'Need For Speed: Payb...',
+    description: 'Deluxe Edition',
+    price: 2.45,
+    img:'./img/card9.webp',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 9,
+    category: 'CARRERA',
+    name: 'Wreckfest',
+    description: 'Complete Edition',
+    price: 2.45,
+    img:'./img/card10.webp',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 10,
+    category: 'CARRERA',
+    name: 'Need For Speed: Heat',
+    description: 'Deluxe Edition',
+    price: 2.45,
+    img:'./img/card11.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 11,
+    category: 'CARRERA',
+    name: 'Forza Horizon 4',
+    description: 'Deluxe Edition',
+    price: 2.45,
+    img:'./img/card12.jpg',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 12,
+    category: 'ACCIÓN',
+    name: 'Gears Of War IV',
+    description: 'Gears Of War IV',
+    price: 2.45,
+    img:'./img/card13.webp',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 13,
+    category: 'ACCIÓN',
+    name: 'Elden Ring',
+    description: 'Elden Ring',
+    price: 2.45,
+    img:'./img/card14.webp',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 14,
+    category: 'ACCIÓN',
+    name: 'God Of War',
+    description: 'Digital Deluxe Edition',
+    price: 2.45,
+    img:'./img/card15.webp',
+    published: '12/12/12',
+    outstanding: false 
+  },
+  {
+    id : 15,
+    category: 'ACCIÓN',
+    name: 'Horizon II',
+    description: 'Forbidden West',
+    price: 2.45,
+    img:'./img/card16.webp',
+    published: '12/12/12',
+    outstanding: false 
+  }
+  ]
